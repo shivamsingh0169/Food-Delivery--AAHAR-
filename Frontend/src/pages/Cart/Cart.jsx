23d1dafd6c42d3f4e8f3ba697dd229423bc2baf0
Kumar Shivam <152880885+shivamsingh0169@users.noreply.github.com>
2025-06-21 21:28:06 +0530

    Update Cart.jsx

--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -27,9 +27,9 @@ const Cart = () => {
                 <div className="cart-items-title cart-items-item">
                   <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>${item.price}</p>
+                  <p>₹{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
+                  <p>₹{item.price * cartItems[item._id]}</p>
                   <p 
                  onClick={()=> removeFromCart(item._id)} className='cross'>X</p>
                 </div>
