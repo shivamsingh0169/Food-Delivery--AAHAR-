2a19001436e0f3ab9785d310eaf833d33cf36a60
Kumar Shivam <152880885+shivamsingh0169@users.noreply.github.com>
2025-06-21 12:56:06 +0530

    Update assets.js

--- admin/admin/src/assets/assets.js
+++ admin/admin/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_image from './profile_image.jpg'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://food-delivery-aahar-backend.onrender.com'
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +15,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-aahar-backend.onrender.com'
+
